422e4d99d81e394e0fcd7b6697c59ae1b89942b0
uganthan2005 <muganthan.aids@citchennai.net>
2026-01-16 13:48:05 +0530

    about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ const timelineEvents = [
     year: "4.0",
     title: "NEURAL NEXUS",
     description: "TALOS 4.0 explored the power of interconnected thinking. Inspired by neural systems, this edition emphasized collaboration, adaptability, and intelligent problem-solving. Every event reflected how modern technology thrives on connections—between people, data, and ideas.",
-    image: "/v3.png" // Placeholder/Fallback
+    image: "/images/inaugration.JPG"
   },
   {
     year: "3.0",
